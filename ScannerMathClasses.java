06cab2097d7efc96116f5da8281d73a870fe06d3
Kaustav Bag <122479158+KaustavBag@users.noreply.github.com>
2023-03-12 19:24:15 +0530

    Add files via upload

---
+++ ScannerMathClasses.java
@@ -0,0 +1,11 @@
+public class ScannerMathClasses {
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        int c=30;
+        double result;
+
+        result=Math.sqrt(Math.pow(a,2)+Math.pow(b,2)+Math.abs(c));
+        System.out.println(result);
+    }
+}
